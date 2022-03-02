f8d1e0465b05977a339727f873582554d46b00de
Cameron Cosby <82902082+camcosby08@users.noreply.github.com>
2022-03-02 16:55:37 -0500

    added menu

---
+++ Rolling Ball 3000/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] KeyCode restartButton;
+
+    //Apply a constant force 
+    void FixedUpdate()
+    {
+        //restart level programming
+        if (Input.GetKey(restartButton))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Rolling Ball 3000/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] Vector3 v3Force;
+
+    [SerializeField] KeyCode positive;
+    [SerializeField] KeyCode negative;
+
+    //Apply a constant force 
+    void FixedUpdate()
+    {
+        if (Input.GetKey(positive))
+        {
+            GetComponent<Rigidbody>().velocity += v3Force;
+        }
+        if (Input.GetKey(negative))
+        {
+            GetComponent<Rigidbody>().velocity -= v3Force;
+        }
+    }
+}
